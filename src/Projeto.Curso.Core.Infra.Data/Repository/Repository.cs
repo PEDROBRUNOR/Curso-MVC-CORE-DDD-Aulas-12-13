ff71994d33aae2a71b501422b78f9c583ea7a404
Pedro Bruno <pedrobrunor@hotmail.com>
2018-06-30 08:39:29 -0300

    alulas12-13

---
+++ src/Projeto.Curso.Core.Infra.Data/Repository/Repository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Projeto.Curso.Core.Domain.Pedido.Interfaces.Repository;
+using Projeto.Curso.Core.Domain.Shared.Entidades;
+using Projeto.Curso.Core.Infra.Data.Context;
+
+namespace Projeto.Curso.Core.Infra.Data.Repository
+{
+    public class Repository<TEntidade> : IRepository<TEntidade> where TEntidade : EntidadeBase
+    {
+        protected ContextEFPedidos Db;
+        protected DbSet<TEntidade> DbSet;
+
+        public Repository(ContextEFPedidos context)
+        {
+            Db = context;
+        }
+
+        public void Adicionar(TEntidade obj)
+        {
+            DbSet.Add(obj);
+        }
+
+        public void Atualizar(TEntidade obj)
+        {
+            DbSet.Update(obj);
+        }
+
+        public void Remover(TEntidade obj)
+        {
+            DbSet.Remove(obj);
+        }
+
+        public IEnumerable<TEntidade> ObterTodos()
+        {
+            return DbSet.ToList();
+        }
+
+        public TEntidade ObterPorId(int id)
+        {
+            return DbSet.AsNoTracking().FirstOrDefault(t => t.Id == id);
+        }
+
+        public IEnumerable<TEntidade> Buscar(Expression<Func<TEntidade, bool>> predicate)
+        {
+            return DbSet.AsNoTracking().Where(predicate);
+        }
+
+        public void Dispose()
+        {
+            Db.Dispose();
+        }
+
+    }
+
+}
---
+++ src/Projeto.Curso.Core.Pedidos/Interfaces/Repository/AgregacaoPedidos/IRepositorioPedidos.cs
@@ -0,0 +1,15 @@
+﻿using Projeto.Curso.Core.Domain.Pedido.AgregacaoPedidos;
+using System.Collections.Generic;
+
+namespace Projeto.Curso.Core.Domain.Pedido.Interfaces.Repository.AgregacaoPedidos
+{
+    public interface IRepositorioPedidos : IRepository<Pedidos>
+    {
+        void AdicionarItensPedidos(ItensPedidos item);
+        void AtulizarItensPedidos(ItensPedidos item);
+        void RemoverItensPedidosPedidos(ItensPedidos item);
+        ItensPedidos ObterItensPedidosPorId(int id);
+        IEnumerable<ItensPedidos> ObterItensPedido(int idpedido);
+
+    }
+}
